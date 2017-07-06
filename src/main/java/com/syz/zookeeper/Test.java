d4d6f0329472c9d40d66a5afe5076a7c3fbadbab
shiyizhen <shiyizhen@10.0.1.170>
2017-07-06 16:09:43 +0800

    。

--- src/main/java/com/syz/zookeeper/Test.java
+++ src/main/java/com/syz/zookeeper/Test.java
@@ -6,10 +6,10 @@ import java.util.Date;
 
 public class Test {
 	public static void main(String[] args) throws ParseException {
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-M-d H:m:s");  
-	    Date date = dateFormat.parse("2017-6-8 14:00:00");  
-	    System.out.println(date.getTime());
-	    date = dateFormat.parse("2017-6-8 14:00:05"); 
-	    System.out.println(date.getTime());
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-M-d H:m:s");
+		Date date = dateFormat.parse("2017-7-02 17:14:00");
+		System.out.println(date.getTime());
+		date = dateFormat.parse("2017-6-22 17:14:05");
+		System.out.println(date.getTime());
 	}
 }
